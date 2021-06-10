367ee48afa623259b91bac526b22c6403d2ffa16
costa <costandinoh@gmail.com>
2021-06-10 21:29:08 +0200

    Created RotatingSprinkler.js

---
+++ src/sprites/RotatingSprinkler.js
@@ -0,0 +1,17 @@
+import Phaser from "../lib/Phaser.js";
+import Sprinkler from "./Sprinkler.js";
+
+export default class RotatingSprinkler extends Sprinkler {
+    constructor(rotatingSprinklerFilePath, rotatingSprinklerRadius, rotatingSprinklerXPosition, rotatingSprinklerYPosition) {
+        super(rotatingSprinklerFilePath, rotatingSprinklerXPosition, rotatingSprinklerYPosition);
+        this.rotatingSprinklerRadius = rotatingSprinklerRadius;
+    }
+
+    getRotatingSprinklerRadius() {
+        return this.rotatingSprinklerRadius;
+    }
+
+    setRotatingSprinklerRadius(rotatingSprinklerRadius) {
+        this.rotatingSprinklerRadius = rotatingSprinklerRadius;
+    }
+}
