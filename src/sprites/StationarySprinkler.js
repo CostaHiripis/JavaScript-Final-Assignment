6e26e2a1beb2c11dfe78ad634134595058c52f84
costa <costandinoh@gmail.com>
2021-06-10 21:29:23 +0200

    Created StationarySprinkler.js

---
+++ src/sprites/StationarySprinkler.js
@@ -0,0 +1,17 @@
+import Phaser from "../lib/Phaser.js";
+import Sprinkler from "./Sprinkler.js";
+
+export default class StationarySprinkler extends Sprinkler{
+    constructor(stationarySprinklerSpan, stationarySprinklerFilePath, stationarySprinklerXPosition, stationarySprinklerYPosition) {
+        super(stationarySprinklerFilePath, stationarySprinklerXPosition, stationarySprinklerYPosition);
+        this.stationarySprinklerSpan = stationarySprinklerSpan;
+    }
+
+    getStationarySprinklerSpan() {
+        return this.stationarySprinklerSpan;
+    }
+
+    setStationarySprinklerSpan(stationarySprinklerSpan) {
+        this.stationarySprinklerSpan = stationarySprinklerSpan;
+    }
+}
