06a96d7f2ba218e19c1680e46b353dccf7430fd7
costa <costandinoh@gmail.com>
2021-06-10 21:29:54 +0200

    Created Item.js

---
+++ src/js/Item.js
@@ -0,0 +1,37 @@
+import Phaser from "../lib/Phaser.js";
+
+export default class Item {
+    constructor(itemName, itemFilePath) {
+        this.itemName = itemName;
+        this.itemFilePath = itemFilePath;
+        this.itemUnlocked = false;
+    }
+
+    getItemName() {
+        return this.itemName;
+    }
+
+    getItemFilePath() {
+        return this.itemFilePath;
+    }
+
+    isItemUnlocked() {
+        return this.itemUnlocked;
+    }
+
+    setItemName(itemName) {
+        this.itemName = itemName;
+    }
+
+    setItemFilePath(itemFilePath) {
+        this.itemFilePath = itemFilePath;
+    }
+
+    unlockItem() {
+        this.itemUnlocked = true;
+    }
+
+    lockItem() {
+        this.itemUnlocked = false;
+    }
+}
