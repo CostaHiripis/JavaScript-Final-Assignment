009a5f54593a846e849cb1aeac8966088e5f9e81
costa <costandinoh@gmail.com>
2021-06-12 14:19:51 +0200

    created MainMenu.js

---
+++ src/scenes/MainMenu.js
@@ -0,0 +1,16 @@
+import Phaser from "../lib/Phaser.js";
+import Scene from "./Scene.js";
+
+export default class MainMenu extends Scene{
+    constructor() {
+        super("MainMenu", "./assets/insertBackgroundImageHere");
+    }
+
+    preload() {
+        super.preLoad();
+    }
+
+    create() {
+        super.create();
+    }
+}
