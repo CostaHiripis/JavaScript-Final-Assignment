543cea067eafc9d5017509f98233b232ca1f4021
costa <costandinoh@gmail.com>
2021-06-12 14:02:35 +0200

    Create Cashoola.js

---
+++ src/sprites/Pickables/Cashoola.js
@@ -0,0 +1,7 @@
+import Pickable from "./Pickable.js";
+
+export default class Cashoola extends Pickable {
+    constructor(cashoolaFilePath, cashoolaXPosition, cashoolaYPosition) {
+        super(cashoolaFilePath, cashoolaXPosition, cashoolaYPosition, "Cashoola");
+    }
+}
